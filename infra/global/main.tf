153ab6451b35f1175e572716745c48b045a4137b
ahalcyon <octopus.1582@gmail.com>
2025-07-22 04:42:33 +0900

    fixup

--- infra/global/main.tf
+++ infra/global/main.tf
@@ -12,3 +12,44 @@ resource "aws_dynamodb_table" "tfstate_lock" {
     type = "S"
   }
 }
+
+# GitHub OIDC Provider
+resource "aws_iam_oidc_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+  client_id_list = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
+
+# IAM Role for GitHub Actions
+resource "aws_iam_role" "github_actions" {
+  name = "github-actions-role-stock-track-alert"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Federated = aws_iam_oidc_provider.github.arn
+        },
+        Action = "sts:AssumeRoleWithWebIdentity",
+        Condition = {
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:ahalcyon/stock-track-alert:*"
+          }
+        }
+      }
+    ]
+  })
+}
+
+# Attach AdministratorAccess policy to the role
+resource "aws_iam_role_policy_attachment" "github_actions_admin" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+output "github_actions_role_arn" {
+  description = "The ARN of the IAM role for GitHub Actions"
+  value       = aws_iam_role.github_actions.arn
+}
